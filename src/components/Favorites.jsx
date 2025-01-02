fdc57e703eb3646d8b7a4be4cea68650f94459c3
Ashikur Rafi <ashikurrafi@gmail.com>
2025-01-03 02:05:20 +0600

    Add 404

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -15,13 +15,19 @@ const Favorites = () => {
     <>
       <h1 className="text-center font-semibold text-xl">Favorite Pokémon</h1>
 
-      <div className="flex justify-center mt-4">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-          {favoritePokemons.map((pokemon) => (
-            <Cards key={pokemon.name} pokemonData={pokemon} />
-          ))}
-        </ul>
-      </div>
+      {favoritePokemons.length === 0 ? (
+        <div className="text-center text-xl text-red-500 mt-8">
+          <h2>404 No Pokémon Found</h2>
+        </div>
+      ) : (
+        <div className="flex justify-center mt-4">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            {favoritePokemons.map((pokemon) => (
+              <Cards key={pokemon.name} pokemonData={pokemon} />
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 };
